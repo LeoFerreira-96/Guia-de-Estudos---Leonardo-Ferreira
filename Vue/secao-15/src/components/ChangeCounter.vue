63937581db0c64dca254012e894be8f83aaa0073
Leonardo Ferreira <leonardo.ferreira96@outlook.com.br>
2020-10-28 11:25:35 -0300

    finalizado secao 15, apenas rever secao 13 e 15 por apresentar erros com vue router e vuex tem algo a ver com export

---
+++ Vue/secao-15/src/components/ChangeCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="inc">adicionar 2</button>
+  <button @click="increase({value: 11})">adicionar 11</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions('numbers', {
+      inc: 'increment',
+      increase: 'increase'
+    })
+  }
+}
+</script>
